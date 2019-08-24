adf7867475f6b037ee90171686c658f03c4c1fff
Stevan Dedovic <stevan@dedovic.com>
2019-08-24 03:08:57 -0500

    add groups and state and channels

--- src/cellular_midi/core.clj
+++ src/cellular_midi/core.clj
@@ -52,9 +52,19 @@
   (filter #(= parent-count (count (alive-neighbours cells %))) (dead-neighbour-cells cells)))
 
 (defn tick [cells]
-  (set/union (set (reproduce cells)) (set (regulate cells))))
+  (set/select
+    (fn [[x y]] (and (>= x 0)
+                     (>= y 0)
+                     (< x 8)
+                     (< y 8)))
+    (set/union (set (reproduce cells)) (set (regulate cells)))))
 
 (def all-cells [0 1 2 3 4 5 6 7 16 17 18 19 20 21 22 23 32 33 34 35 36 37 38 39 48 49 50 51 52 53 54 55 64 65 66 67 68 69 70 71 80 81 82 83 84 85 86 87 96 97 98 99 100 101 102 103 112 113 114 115 116 117 118 119])
+
+(defn clear-all [sink]
+  (doseq [n all-cells]
+    (m/midi-note-off sink n)))
+
 (defn cell-to-note [[x y]]
   (let [note-val (+ x (* y 16))]
     note-val))
@@ -62,27 +72,114 @@
 (defn note-to-cell [n]
   (let [x (mod n 16)
         y (int (/ n 16))]
-    (println [x y])
     [x y]))
 
-(defn draw-cells [sink cells]
-  (doseq [note all-cells] (m/midi-note-off sink note))
-  (doseq [cell cells] (m/midi-note-on sink (cell-to-note cell) 127)))
+(defn draw-cells 
+  ([sink cells]
+    (draw-cells sink cells 127))
+  ([sink cells vel]
+    (doseq [note all-cells] (m/midi-note-off sink note))
+    (doseq [cell cells] (m/midi-note-on sink (cell-to-note cell) vel))))
+
+(defn draw-groups-init [sink]
+  (m/midi-note-on sink (cell-to-note [8 0]) 16))
+
+(defn group-to-vel
+  ([group]
+    (group-to-vel group :low))
+  ([group amt]
+    (cond
+      (and (= group 0) (= amt :low))
+      16
+      
+      (and (= group 0) (= amt :high))
+      32
+      
+      :else 0)))
 
 (def lp-out (m/midi-out))
 (def lp-in (m/midi-in))
 
-(def current-board (atom #{[2 2] [3 2] [4 2]}))
+(def state (atom {:board #{[2 2] [3 2] [4 2]}
+                  :groups [#{} #{} #{} #{} #{} #{} #{} #{}]
+                  :render :board}))
 
-(m/midi-handle-events
-  lp-in
-  (fn [{note :note cmd :cmd vel :vel} ts]
-    (when (and (= 0 vel) (= 144 cmd) (some #(= note %) all-cells))
-      (swap! current-board conj (note-to-cell note)))))
 
-(draw-cells lp-out @current-board)
+(defn handle-event [state event]
+  (let [board (:board state)
+        render (:render state)
+        {note :note cmd :cmd vel :vel} event]
+    (cond 
+      ;; cell
+      (and (= 0 vel) (= 144 cmd) (some #(= note %) all-cells))
+      (cond 
+        (= render :board)
+        (update state :board conj (note-to-cell note))
+
+        (= render :group)
+        (let [cell (note-to-cell note)
+              group (:group state)
+              cells (nth (:groups state) group)]
+          (if (set/subset? [cell] cells)
+            (do
+              (m/midi-note-off lp-out note)
+              (update-in state [:groups group] (partial set/select #(not (= note (cell-to-note %))))))
+            (do
+              (m/midi-note-on lp-out note (group-to-vel group))
+              (update-in state [:groups group] conj cell)))))
+
+      ;; group 1
+      (and (= 0 vel) (= 144 cmd) (= 8 note))
+      (do
+        (m/midi-note-on lp-out (cell-to-note [8 0]) 32)
+        (-> state
+          (assoc :render :group)
+          (assoc :group 0)))
+
+      ;; top left
+      (and (= 0 vel) (= 104 note) (= 176 cmd))
+      (do
+        (draw-groups-init lp-out)
+        (assoc state :render :board))
+        
+      (= 0 vel)
+      (do (println "note: " note " cmd: " cmd)
+          state)
+      
+      :else state)))
+
+(defn update-state [state]
+  (let [board-notes (set (map cell-to-note (:board state)))
+        groups-notes (map #(set (map cell-to-note %)) (:groups state))]
+    (doseq [[idx cells] (map-indexed vector groups-notes)]
+      (let [hits (set/intersection board-notes cells)
+            any? (> (count hits) 0)]
+        (println any?)
+        (if any?
+          (m/midi-note-on lp-out (cell-to-note [8 idx]) (group-to-vel idx :high))
+          (m/midi-note-on lp-out (cell-to-note [8 idx]) (group-to-vel idx :low))))))
+  (update state :board tick))
+
+(defn draw [state]
+  (let [render (:render state)]
+    (cond
+      (= render :group)
+      (let [group (:group state)
+            cells (nth (:groups state) group)
+            vel (group-to-vel group)]
+        (draw-cells lp-out cells vel))
+
+      (= render :board)
+      (draw-cells lp-out (:board state)))))
+
+;; execute event handler on midi events
+(m/midi-handle-events 
+  lp-in 
+  (fn [event _] (swap! state handle-event event)))
+
+(draw @state)
 (loop []
-  (let [new-board (swap! current-board tick)]
-    (draw-cells lp-out new-board)
+  (let [new-state (swap! state update-state)]
+    (draw new-state)
     (Thread/sleep 350)
     (recur)))
